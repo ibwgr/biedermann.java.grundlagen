a318807b9e2cbb5cd100e1e7d0ed3663b45b33c3
Dieter Biedermann <biedermann.dieter@powersurf.li>
2016-09-22 20:40:33 +0200

    first commit

---
+++ src/Kapitel8/Student.java
@@ -0,0 +1,47 @@
+package Kapitel8;
+
+/**
+ * Created by dieterbiedermann on 20.09.16.
+ */
+public class Student {
+
+    private String name;
+
+    private int nummer;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNummer() {
+        return nummer;
+    }
+
+    public void setNummer(int nummer) {
+        int alteNummer = this.nummer;
+        this.nummer = nummer;
+        if (!validateNummer()) {
+            this.nummer = alteNummer;
+        }
+    }
+
+    public boolean validateNummer() {
+        return (nummer >= 10000 && nummer <= 99999 && nummer % 2 != 0);
+    }
+
+    public boolean checkSomething() {
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", nummer=" + nummer +
+                '}';
+    }
+}
---
+++ src/Kapitel8/TestStudent.java
@@ -0,0 +1,17 @@
+package Kapitel8;
+
+import Kapitel8.Student;
+
+/**
+ * Created by dieterbiedermann on 20.09.16.
+ */
+public class TestStudent {
+    public static void main(String[] args) {
+        Student student1 = new Student();
+
+        student1.setName("Hans");
+        student1.setNummer(11111);
+        System.out.println((student1.validateNummer() ? "true" : "false"));
+        System.out.println(student1);
+    }
+}
